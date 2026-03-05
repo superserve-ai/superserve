e8b98b03a6320b425fdc81f32c2737755ff07b50
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2026-03-05 22:11:27 +0530

    console: app setup

---
+++ apps/console/next.config.ts
@@ -0,0 +1,9 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  reactCompiler: true,
+  trailingSlash: true,
+  reactStrictMode: true,
+};
+
+export default nextConfig;
